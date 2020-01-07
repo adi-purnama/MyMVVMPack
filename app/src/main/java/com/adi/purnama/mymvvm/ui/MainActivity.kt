37674ab58e6a26ff929360c66ba958234be5a2ab
adi-purnama <adipurnama.ohi@gmail.com>
2020-01-07 11:49:21 +0700

    initial library and structure package

---
+++ app/src/main/java/com/adi/purnama/mymvvm/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.adi.purnama.mymvvm.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.adi.purnama.mymvvm.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
