6cc3dc3a51f61434f73a5d9f935edc1adaf7d6a4
adi-purnama <adipurnama.ohi@gmail.com>
2020-01-09 15:11:04 +0700

    setting material theme

--- app/src/main/java/com/adi/purnama/mymvvm/ui/MainActivity.kt
+++ app/src/main/java/com/adi/purnama/mymvvm/ui/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.adi.purnama.mymvvm.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.adi.purnama.mymvvm.R
 
 class MainActivity : AppCompatActivity() {
